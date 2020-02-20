0f2c54d5b91c3b9d37a6b90b861fcb50a70730f3
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-02-20 02:23:32 -0800

    Updates

---
+++ precache-manifest.73f13abb5d92f717fb7b102e78fde0d9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e783d314e82b68f181b9771d905b5846",
+    "url": "/pokemon-team-creator/index.html"
+  },
+  {
+    "revision": "545540e1a30169f6a68f",
+    "url": "/pokemon-team-creator/static/css/main.6d3f02fe.chunk.css"
+  },
+  {
+    "revision": "b842b420464f60a9c6e7",
+    "url": "/pokemon-team-creator/static/js/2.72fa39b7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/pokemon-team-creator/static/js/2.72fa39b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "545540e1a30169f6a68f",
+    "url": "/pokemon-team-creator/static/js/main.7a70836d.chunk.js"
+  },
+  {
+    "revision": "9c31efdba66f4eb10db8",
+    "url": "/pokemon-team-creator/static/js/runtime-main.79d2975b.js"
+  },
+  {
+    "revision": "484ea339f7a4267ce3d4632e4068ccfa",
+    "url": "/pokemon-team-creator/static/media/pokeball.484ea339.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/pokemon-team-creator/precache-manifest.73f13abb5d92f717fb7b102e78fde0d9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pokemon-team-creator/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
